4674aa8764559a881df9f21061b0e1f2cd414ac0
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2023-10-27 15:47:44 +0900

    firebase init

---
+++ src/firebase/server.ts
@@ -0,0 +1,16 @@
+
+import { initializeApp, cert } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
+
+import serviceAccount from '../../secret-key.json'
+
+// firebase-admin SDK によって認証することで，セキュリティルールを無視して通信する
+const app = initializeApp({
+    credential: cert({
+        projectId: serviceAccount.project_id,
+        privateKey: serviceAccount.private_key,
+        clientEmail: serviceAccount.client_email,
+    })
+})
+
+export const adminDB = getFirestore(app)
