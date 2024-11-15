002e249c2c526f6cdf72649c326b10798c0b5032
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2024-11-15 19:14:18 +0900

    fix: resolve dependency problem

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.m?js$/,
+      resolve: {
+        fullySpecified: false, // ESMの拡張子関連問題を無視
+      },
+    });
 
-module.exports = nextConfig
+    config.module.rules.push({
+      test: /\.js$/,
+      include: /node_modules\/undici/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
